2661a95e5e9a967fb1de076a5cb5ac51e87be227
Phil Nelson <test@nothing.nowhere>
2026-09-16 14:54:31 -0500

    Add empty SQL submission templates for Week 3 Lab and Homework

---
+++ week03/lab_solutions.sql
@@ -0,0 +1,16 @@
+-- ==========================================
+-- STUDENT NAME: [First Last]
+-- STUDENT ID: [ID Number]
+-- ==========================================
+
+-- Ticket 1
+
+
+-- Ticket 2
+
+
+-- Ticket 3
+
+
+-- Ticket 4
+
